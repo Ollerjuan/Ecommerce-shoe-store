a80a74ba1bdc7aadb35961390a928e350245fc0a
Juan <juan_oller@yahoo.com>
2022-06-05 12:24:29 -0500

    added styling to produts

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,16 +2,24 @@
 import React from 'react'
 import Product from '../Components/Product'
 
+
 const Home = () => {
+   
   return <div className='homescreen'>
-    <div className='car_pic'/>
+
+      <img className="banner" src="http://s3.nikecdn.com/events-platform/pre-prod/NET_Admin/header/2017-05-20_657_Banner_Blue.jpeg"/>
+
        <h1 className='Current_list'>Current Shoes for sale</h1> 
-       <div screen="homescreen_products">
+       <div className="homescreen_products">
+          <Product />
+          <Product />
+          <Product />
           <Product />
           <Product />
           <Product />
           
        </div>
+       
     </div>
   
 }
