16c174977a18caf607da4edf3b7050fa8444a512
Juan <juan_oller@yahoo.com>
2022-06-05 08:35:21 -0500

    homepage gif

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,10 @@
-//  import './HomeScreen.css'
+ import './HomeScreen.css'
 import React from 'react'
 import Product from '../Components/Product'
 
 const Home = () => {
-  return <div className='homeScreen'>
-       {/* <div className='bus_pic'> */}
-        <img className='rolling' src="https://media0.giphy.com/media/clufgbyIA9Q2a23uQF/giphy.gif?cid=790b7611216ab7e300ed29c728e2840cb284f88f345355e4&rid=giphy.gif&ct=g" alt=""/>
-      {/* </div> */}
+  return <div className='homescreen'>
+    <div className='car_pic'/>
        <h1 className='Current_list'>Current Shoes for sale</h1> 
        <div screen="homescreen_products">
           <Product />
