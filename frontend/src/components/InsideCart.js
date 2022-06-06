91da590994a2142efab410b4f229888383137514
Juan <juan_oller@yahoo.com>
2022-06-06 14:38:32 -0500

    adding css for cart component

--- frontend/src/components/InsideCart.js
+++ frontend/src/components/InsideCart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './InsideCart.css';
 import { Link } from 'react-router-dom'
 
 export const InsideCart = () => {
@@ -7,8 +8,16 @@ export const InsideCart = () => {
         <div className='cart_item'>
             <img src="https://images.stockx.com/images/Adidas-Yeezy-Boost-350-V2-Cream-White-1-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1606322741" alt="logo"/>
         </div>
-      
+        <Link to={`/product/${111}`} className="product_one">
+            <p> Product 1</p>
+
+        </Link>
+        <p className='cartitem_price'> 250$</p>
+
+        <button className='delete'> Delete
+        </button>
     </div>
   )
 }
 export default InsideCart
+
