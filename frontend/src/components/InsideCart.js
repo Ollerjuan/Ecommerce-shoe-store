07c33e7abf2396b0363db691fc05079a2904ab9c
Juan <juan_oller@yahoo.com>
2022-06-06 13:59:35 -0500

    adding cart component to render products inside the cart

---
+++ frontend/src/components/InsideCart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const InsideCart = () => {
+  return (
+    <div className="cartitem">
+        <div className='cart_item'>
+            <img src="https://images.stockx.com/images/Adidas-Yeezy-Boost-350-V2-Cream-White-1-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1606322741" alt="logo"/>
+        </div>
+      
+    </div>
+  )
+}
+export default InsideCart
