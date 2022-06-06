006e785ce0989b37e83dafd80bd0d0232109a192
Juan <juan_oller@yahoo.com>
2022-06-06 10:14:04 -0500

    adding css to product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -4,21 +4,34 @@ import './ProductScreen.css';
 
 function View() {
 
-  return <div className='productscreen'>
+  return(
+   <div className='productscreen'>
     <div className='left_screen'>
       <img src='https://images.stockx.com/images/Adidas-Yeezy-Boost-350-V2-Cream-White-1-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1606322741' alt="loading" />
   
-    </div>
       <div className='left_info'>
         <p className='shoe_name'> Yeezy</p>
         <p className='price'>Price: $250.00</p>
         <p className='shoe_size'>Size: 8</p>
       </div>
+    </div>
   
-
+    <div className='right_screen'>
+        <div className='right_info'>
+          <p>
+            Price: <span> 250$</span>
+          </p>
+          <p>
+            Status: <span> In Stock</span>
+          </p>
+          <p>
+            <button type="button"> Add to Cart</button>
+          </p>
+        </div>
+    </div>
   </div>
- 
-
+  )
+  
 }
 
 export default View;
