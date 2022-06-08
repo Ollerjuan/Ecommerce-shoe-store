493cb4dc13311823c1476f2bab92e7ac25ae7a3b
Juan <juan_oller@yahoo.com>
2022-06-07 19:28:49 -0500

    added login function using auth0

--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 
+const Loginbutton = () => {
+    const { loginWithRedirect } = useAuth0 ();
 
-const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
-    return (
-        <button onClick={() => loginWithRedirect()}>
-            Log in 
-        </button>
-    )
+  return (
+    <button onClick={()=> loginWithRedirect()}>
+        Log in
+    </button>
+  )
 }
 
-
-export default LoginButton;
+export default Loginbutton;
