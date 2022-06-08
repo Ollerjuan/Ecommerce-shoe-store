69f21562e2631ea4155d96cd83c11f08e17d6262
Juan <juan_oller@yahoo.com>
2022-06-08 09:21:41 -0500

    added login/logout function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,25 @@ import { Routes } from 'react-router-dom'
 //components
 import Nav from './Components/Nav';
 // import Loginbutton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
 
 //screens added
 import Home from './screens/HomeScreen';
 import View from './screens/ProductScreen';
 import Cart from './screens/CartScreen';
 import Login from './screens/LoginScreen';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <Router>
       <Nav />
-          {/* <Loginbutton /> */}
+        <Profile />
+        {/* <Loginbutton /> */}
+        <LogoutButton />
         <Routes>
-          <Route path="/LoginScreen" element={<Login/>} />
-          <Route path="/" element={<Home/>}/>       
+          <Route path="/" element={<Login/>} />
+          <Route path="Home/" element={<Home/>}/>    
           <Route path="cart/" element={<Cart />} />
           <Route path="productScreen/" element={<View />} />
           <Route path="./Product/:id" Component={<View />}/>
